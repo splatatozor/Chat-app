9e8697e7461b0192bc6f1410cdd89e13a1f0d9c2
samuel Kabbaj <kabbaj.samuel@gmail.com>
2018-06-05 20:27:32 +0200

    [feat] - add design on first page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { ChatComponent } from './chat/chat.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideBarComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule
