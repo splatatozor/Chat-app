1b56bac71c4211fe7a51ae119e903fb49ac425ec
samuel Kabbaj <kabbaj.samuel@gmail.com>
2018-06-07 15:52:09 +0200

    [feat] - add profile page without css Beurkgit add .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { SideBarComponent } from './side-bar/side-bar.component';
 import { ChatComponent } from './chat/chat.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SideBarComponent,
-    ChatComponent
+    ChatComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule
